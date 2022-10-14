14c840d40fba5227be8d77d62d237af919e10675
Hlalela29 <hlalelamasuluke@gmail.com>
2022-10-13 18:42:09 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,53 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
-*main- Serves as the entry point for the program
-*Return: By default returns zero if no errors otherwise returns non-zero value.
-*/
+ * main - Entry Point
+ *
+ * Return: Always 0 (Successfull)
+ */
+
 int main(void)
 {
-    puts("
-"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
 }
-
-
-#include <stdio.h>
-/**
-*main- Serves as the entry point for the program
-*Return: By default returns zero if no errors otherwise returns non-zero value.
-*/
-int main(void)
-{
-    puts("
-"Programming is like building a multilingual puzzle");
-    return (0);
-}
-
-
-#include <stdio.h>
-/**
-*main- Serves as the entry point for the program
-*Return: By default returns zero if no errors otherwise returns non-zero value.
-*/
-int main(void)
-{
-    puts("
-"Programming is like building a multilingual puzzle");
-    return (0);
-}
-
-
-#include <stdio.h>
-/**
-*main- Serves as the entry point for the program
-*Return: By default returns zero if no errors otherwise returns non-zero value.
-*/
-int main(void)
-{
-    puts("
-"Programming is like building a multilingual puzzle");
-    return (0);
-}
-
-
